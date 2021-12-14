7da46e52c663c379b2aef4b3294c2a9ce2c03a3f
Tani Kaye <1tanikaye@gmail.com>
2021-12-14 13:55:15 -0500

    commit tree fa7e30924e9420d8ad3f72674ba4835441784c95

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :clickbait
+
+  def clickbait
+    if title.include("Won't Believe") == false &&
+      title.include("Secret") == false &&
+      title.include("Top [number]") == false &&
+      title.include("Guess") == false
+      errors.add(:title, "needs clickbaity title")
+    end
+  end
+
 end
